7bc88d9c9c74e8894f581d618e878202ff6352e7
Thomas Egret <barbarior59@hotmail.com>
2013-11-19 14:36:04 +0100

    puma conf again

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@
 threads 4,8
 
 bind  "unix:///home/institut/egret/ocs/shared/tmp/puma/ocs-puma.sock"
-pidfile "/home/institut/egret/ocs/current/tmp/pids"
-state_path "/home/institut/egret/ocs/current/tmp/sockets"
+pidfile "/home/institut/egret/ocs/current/tmp/puma/pid"
+state_path "/home/institut/egret/ocs/current/tmp/puma/state"
 
 stdout_redirect '/home/institut/egret/ocs/current/log/puma.access.log', '/home/institut/egret/ocs/current/log/puma.error.log', true
 
