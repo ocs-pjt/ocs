7f2f5aa4932155143b7c3ffc89a7f664b9e4c687
Thomas Egret <barbarior59@hotmail.com>
2013-11-19 14:28:05 +0100

    Puma conf

--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,11 @@
 
 threads 4,8
 
-# bind  "unix:///home/institut/egret/ocs/shared/tmp/puma/ocs-puma.sock"
-# pidfile "/home/institut/egret/ocs/current/tmp/puma/pid"
-# state_path "/home/institut/egret/ocs/current/tmp/puma/state"
+bind  "unix:///home/institut/egret/ocs/shared/tmp/puma/ocs-puma.sock"
+pidfile "/home/institut/egret/ocs/current/tmp/pids"
+state_path "/home/institut/egret/ocs/current/tmp/sockets"
 
-# stdout_redirect '/home/institut/egret/ocs/current/log/puma.access.log', '/home/institut/egret/ocs/current/log/puma.error.log', true
+stdout_redirect '/home/institut/egret/ocs/current/log/puma.access.log', '/home/institut/egret/ocs/current/log/puma.error.log', true
 
 activate_control_app
-# daemonize true
+daemonize true
