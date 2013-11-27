14a7a0e40c0085e817b204690d087d041cff9d13
Thomas Egret <thomas.egret@gmail.com>
2013-11-27 22:34:29 +0100

    To pass Travis

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,8 +234,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, Figaro.env.facebook_id, Figaro.env.facebook_secret
-  config.omniauth :google_oauth2, Figaro.env.google_id, Figaro.env.google_secret, { access_type: "offline", approval_prompt: "" }
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], { access_type: "offline", approval_prompt: "" }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
