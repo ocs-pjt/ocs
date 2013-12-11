815f736ccd92533fe40afc3dbc9eba91d9de85ef
Thomas Egret <barbarior59@hotmail.com>
2013-12-11 17:03:15 +0100

    Repair all forms & modals with Bootstrap 3 only error display still needs some fixes

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,3 +1,28 @@
+# HACK simple_form to support Boostrap 3
+inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+]
+ 
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+ 
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.push('form-control')
+    end
+  end
+ 
+  Object.const_set(input_type, new_class)
+end
+
+
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   # Wrappers are used by the form builder to generate a
@@ -5,48 +30,102 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
-    hint_class: :field_with_hint, error_class: :field_with_errors do |b|
-    ## Extensions enabled by default
-    # Any of these extensions can be disabled for a
-    # given input by passing: `f.input EXTENSION_NAME => false`.
-    # You can make any of these extensions optional by
-    # renaming `b.use` to `b.optional`.
-
-    # Determines whether to use HTML5 (:email, :url, ...)
-    # and required attributes
-    b.use :html5
-
-    # Calculates placeholders automatically from I18n
-    # You can also pass a string as f.input placeholder: "Placeholder"
-    b.use :placeholder
-
-    ## Optional extensions
-    # They are disabled unless you pass `f.input EXTENSION_NAME => :lookup`
-    # to the input. If so, they will retrieve the values from the model
-    # if any exists. If you want to enable the lookup for any of those
-    # extensions by default, you can change `b.optional` to `b.use`.
-
-    # Calculates maxlength from length validations for string inputs
-    b.optional :maxlength
-
-    # Calculates pattern from format validations for string inputs
-    b.optional :pattern
-
-    # Calculates min and max from length validations for numeric inputs
-    b.optional :min_max
-
-    # Calculates readonly automatically from readonly attributes
-    b.optional :readonly
-
-    ## Inputs
-    b.use :label_input
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :error, wrap_with: { tag: :span, class: :error }
+  config.wrappers :bootstrap3, tag: 'div', class: 'row' do |row|
+    row.wrapper tag: 'div', class: 'form-group col-md-4', error_class: 'has-error',
+        defaults: { input_html: { class: 'default_class' } } do |b|
+      
+      b.use :html5
+      b.use :min_max
+      b.use :maxlength
+      b.use :placeholder
+      
+      b.optional :pattern
+      b.optional :readonly
+      
+      b.use :label_input
+      b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+      b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+    end
   end
 
+  config.wrappers :bootstrap3_horizontal, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '}, wrapper_html: { class: "col-lg-10 col-md-10"} } do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label
+    b.wrapper :right_column, tag: :div do |component|
+      component.use :input
+    end
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+  config.wrappers :group, tag: 'div', class: "form-group", error_class: 'has-error',
+      defaults: { input_html: { class: 'default-class '} }  do |b|
+
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+
+    b.optional :pattern
+    b.optional :readonly
+
+    b.use :label
+    b.use :input, wrap_with: { class: "input-group" }
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+
+  # config.wrappers :default, class: :input,
+  #   hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+  #   ## Extensions enabled by default
+  #   # Any of these extensions can be disabled for a
+  #   # given input by passing: `f.input EXTENSION_NAME => false`.
+  #   # You can make any of these extensions optional by
+  #   # renaming `b.use` to `b.optional`.
+
+  #   # Determines whether to use HTML5 (:email, :url, ...)
+  #   # and required attributes
+  #   b.use :html5
+
+  #   # Calculates placeholders automatically from I18n
+  #   # You can also pass a string as f.input placeholder: "Placeholder"
+  #   b.use :placeholder
+
+  #   ## Optional extensions
+  #   # They are disabled unless you pass `f.input EXTENSION_NAME => :lookup`
+  #   # to the input. If so, they will retrieve the values from the model
+  #   # if any exists. If you want to enable the lookup for any of those
+  #   # extensions by default, you can change `b.optional` to `b.use`.
+
+  #   # Calculates maxlength from length validations for string inputs
+  #   b.optional :maxlength
+
+  #   # Calculates pattern from format validations for string inputs
+  #   b.optional :pattern
+
+  #   # Calculates min and max from length validations for numeric inputs
+  #   b.optional :min_max
+
+  #   # Calculates readonly automatically from readonly attributes
+  #   b.optional :readonly
+
+  #   ## Inputs
+  #   b.use :label_input
+  #   b.use :hint,  wrap_with: { tag: :span, class: :hint }
+  #   b.use :error, wrap_with: { tag: :span, class: :error }
+  # end
+
   # The default wrapper to be used by the FormBuilder.
-  config.default_wrapper = :default
+  config.default_wrapper = :bootstrap3
 
   # Define the way to render check boxes / radio buttons with labels.
   # Defaults to :nested for bootstrap config.
@@ -95,7 +174,7 @@ SimpleForm.setup do |config|
   # config.label_text = lambda { |label, required| "#{required} #{label}" }
 
   # You can define the class to use on all labels. Default is nil.
-  config.label_class = 'control-label'
+  # config.label_class = 'control-label'
 
   # You can define the class to use on all forms. Default is simple_form.
   # config.form_class = :simple_form
@@ -141,5 +220,5 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  # config.input_class = nil
+  # config.input_class = "form-control"
 end
