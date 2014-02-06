9560154a4604584ecf35507c0d74b2e94bb0546a
Thomas Egret <barbarior59@hotmail.com>
2014-02-06 16:16:55 +0100

    Various style changes [ci skip]

---
+++ config/initializers/overrides.rb
@@ -0,0 +1,23 @@
+module DeviseControllerFlashMessage
+  # This method is called when this mixin is included
+  def self.included klass
+    # klass here is our DeviseController
+
+    klass.class_eval do
+      remove_method :set_flash_message
+    end
+  end
+
+  protected 
+  def set_flash_message(key, kind, options = {})
+    if key == 'alert'
+      key = 'error'
+    elsif key == 'notice'
+      key = 'success'
+    end
+    message = find_message(kind, options)
+    flash[key] = message if message.present?
+  end
+end
+
+DeviseController.send(:include, DeviseControllerFlashMessage)
